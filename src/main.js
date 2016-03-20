2b5f32f9ce276ef4a19f2301dd20c4fb336ec306
Tom Wu <tom76kimo@yahoo-inc.com>
2016-03-20 23:57:19 +0800

    add app bar

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ import { syncHistoryWithStore } from 'react-router-redux'
 import makeRoutes from './routes'
 import Root from './containers/Root'
 import configureStore from './redux/configureStore'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
+injectTapEventPlugin()
 // Configure history for react-router
 const browserHistory = useRouterHistory(createBrowserHistory)({
   basename: __BASENAME__
--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -1,10 +1,17 @@
 /* @flow */
 import React from 'react'
+import AppBar from 'material-ui/lib/app-bar'
+import Colors from 'material-ui/lib/styles/colors'
+import IconButton from 'material-ui/lib/icon-button'
+import PermIdentity from 'material-ui/lib/svg-icons/action/perm-identity'
 
 export class HomeView extends React.Component {
   render () {
     return (
       <div>
+        <AppBar
+          title='讀經列車'
+          iconElementRight={<IconButton><PermIdentity color={Colors.white} /></IconButton>} />
         <h1>Your New Project</h1>
       </div>
     )
