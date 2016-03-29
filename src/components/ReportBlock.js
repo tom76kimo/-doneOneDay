ffa8ec5bd05f33002f75b8f71b47b33a32969f8e
Tom Wu <tom76kimo@yahoo-inc.com>
2016-03-29 17:51:22 +0800

    add feature of confirming reading

--- src/components/ReportBlock.js
+++ src/components/ReportBlock.js
@@ -5,10 +5,14 @@ import CardHeader from 'material-ui/lib/card/card-header'
 import CardActions from 'material-ui/lib/card/card-actions'
 import CardText from 'material-ui/lib/card/card-text'
 import Check from 'material-ui/lib/svg-icons/navigation/check'
+import CheckCircle from 'material-ui/lib/svg-icons/action/check-circle'
 import RaisedButton from 'material-ui/lib/raised-button'
 import Firebase from 'firebase'
 import LinearProgress from 'material-ui/lib/linear-progress'
 import request from 'superagent'
+import Dialog from 'material-ui/lib/dialog'
+import Colors from 'material-ui/lib/styles/colors'
+import IconButton from 'material-ui/lib/icon-button'
 
 const FACEBOOK_LOAD_FINISHED = 0
 const PROGRESS_LOAD_FINISHED = 0
@@ -18,23 +22,24 @@ class ReportBlock extends React.Component {
     super(props)
     this.state = {
       progress: null,
-      userData: null
+      userData: null,
+      modalOpen: false,
+      modalActions: [],
+      modalBody: (<LinearProgress mode="indeterminate"/>),
+      modalTitle: '紀錄中'
     }
+    this.clickHasRead = this.clickHasRead.bind(this)
+    this.hasReadFinished = this.hasReadFinished.bind(this)
+    this.disabledConfirmButton = this.disabledConfirmButton.bind(this)
   }
   render () {
-    let confirmButton = (
-      <RaisedButton
-        label="等待讀取中"
-        disabled
-        icon={<Check />} />
-    )
+    let confirmButton
     if (this.state.progress) {
-      confirmButton = (
-        <RaisedButton
-          label="已讀完，點我！"
-          primary
-          icon={<Check />} />)
+      confirmButton = this.confirmButton()
+    } else {
+      confirmButton = this.disabledConfirmButton()
     }
+
     let progressDescription = (
       <div>
         <span style={{marginLeft: '10px', fontSize: '20px'}}>歡迎來到讀經列車，正在為您讀取資料中...</span>
@@ -82,6 +87,13 @@ class ReportBlock extends React.Component {
             {confirmButton}
           </CardActions>
         </Card>
+        <Dialog
+          title={this.state.modalTitle}
+          actions={this.state.modalActions}
+          open={this.state.modalOpen}
+          modal >
+          {this.state.modalBody}
+        </Dialog>
       </div>
     )
   }
@@ -112,6 +124,78 @@ class ReportBlock extends React.Component {
       self.setState({
         userData: res.body
       })
+    })
+  }
+
+  disabledConfirmButton () {
+    return (
+      <RaisedButton
+        label="等待讀取中"
+        disabled
+        icon={<Check />} />)
+  }
+
+  confirmButton () {
+    return (
+      <RaisedButton
+        label="已讀完，點我！"
+        primary
+        onMouseDown={this.clickHasRead}
+        icon={<Check />} />)
+  }
+
+  clickHasRead () {
+    const self = this
+    this.setState({
+      modalOpen: true
+    })
+    if (!this.state.userData) {
+      request.get('/userData').end((err, res) => {
+        if (err) {
+          return
+        }
+        if (!res.body) {
+          // should login facebook
+          self.setState({
+            modalTitle: '您尚未登入，請先登入',
+            modalBody: (
+              <a href="/auth/facebook" style={{
+                display: 'block',
+                width: 70,
+                marginRight: 'auto',
+                marginLeft: 'auto'
+              }}>
+                <IconButton>
+                  <div className="fb-icon-black">
+                    <i className="fa fa-facebook-official"></i>
+                  </div>
+                </IconButton>
+              </a>)
+          })
+        }
+      })
+    } else {
+      const firebaseRef = new Firebase('https://bible-train.firebaseio.com/')
+      firebaseRef.child('record').child(1459008000000).child(this.state.userData.id).set({
+        userName: this.state.userData.name,
+        createTime: Firebase.ServerValue.TIMESTAMP
+      }, (err) => {
+        if (!err) {
+          self.setState({
+            modalTitle: '記錄完畢！',
+            modalBody: (<CheckCircle color={Colors.green400} style={{width: 50, height: 50}}/>),
+            modalActions: [
+              <RaisedButton primary label="記錄完畢" onTouchTap={this.hasReadFinished}/>
+            ]
+          })
+        }
+      })
+    }
+  }
+
+  hasReadFinished () {
+    this.setState({
+      modalOpen: false
     })
   }
 }
